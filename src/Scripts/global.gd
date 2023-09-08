a4d3b692716a363134a719185f2c3341fbd3c1ab
Honzapkcz <honzapkc@gmail.com>
2023-09-08 16:20:51 +0200

    Rewrote player scripts to add joistick

--- src/Scripts/global.gd
+++ src/Scripts/global.gd
@@ -2,3 +2,5 @@ extends Node
 
 signal add_bullet(bullet: Object)
 signal hurt_player()
+signal fire_bullet()
+signal move_player(vel: Vector2)
--- src/Scripts/player.gd
+++ src/Scripts/player.gd
@@ -5,26 +5,24 @@ class_name Player
 @export var JUMP_VELOCITY = -400.0
 @export var FRICTION = 5.0
 
-var xVel = 0
-var yVel = 0
-#var xScl = 0
-#var yScl = 0
+var input_vel: Vector2
+var joist_vel: Vector2
 var hp = 3
 var bullet = load("res://Scenes/bullet.tscn")
 
+func _ready():
+	Global.connect("fire_bullet", fire_bullet)
+	Global.connect("move_player", player_move)
+
 func _physics_process(delta):
-	xVel = Input.get_axis("Left", "Right")
-	yVel = Input.get_axis("Up", "Down")
-	velocity.x = move_toward(velocity.x, xVel * SPEED * delta * 10000, FRICTION)
-	velocity.y = move_toward(velocity.y, yVel * SPEED * delta * 10000, FRICTION)
-	#xScl = move_toward(scale.x, (abs(xVel) - 0.5) / 8, FRICTION)
+	input_vel.x = Input.get_axis("Left", "Right")
+	input_vel.y = Input.get_axis("Up", "Down")
+	
+	velocity.x = move_toward(velocity.x, input_vel.x * SPEED * delta * 10000, FRICTION)
+	velocity.y = move_toward(velocity.y, input_vel.y * SPEED * delta * 10000, FRICTION)
+	
 	if Input.is_action_just_pressed("Fire") and $Timer.is_stopped():
-		var bul = bullet.instantiate()
-		bul.position = Vector2(position.x, position.y - 180)
-		bul.scale = Vector2(0.2, 0.2)
-		Global.emit_signal("add_bullet", bul)
-		$Timer.start()
-		
+		fire_bullet()
 
 	move_and_slide()
 	
@@ -32,10 +30,20 @@ func _physics_process(delta):
 		position.x = 0
 	elif position.x > 1080:
 		position.x = 1080
-	if position.y < 0:
-		position.y = 0
+	if position.y < 1000:
+		position.y = 1000
 	elif position.y > 1920:
 		position.y = 1920
+
+func fire_bullet():
+	var bul = bullet.instantiate()
+	bul.position = Vector2(position.x, position.y - 180)
+	bul.scale = Vector2(0.2, 0.2)
+	Global.emit_signal("add_bullet", bul)
+	$Timer.start()
+
+func player_move(vec: Vector2):
+	joist_vel = vec
 
 func on_die():
 	$Sprite2D.visible = false
