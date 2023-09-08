6a9eb0c4002862c4d515239ed741de1e6b5f979a
Honzapkcz <honzapkc@gmail.com>
2023-09-08 16:32:07 +0200

    zebricek_menu.gd: Added support for Android back button

--- src/Scripts/zebricek_menu.gd
+++ src/Scripts/zebricek_menu.gd
@@ -1,6 +1,10 @@
 extends Control
 
 
+func _notification(what):
+	if what == NOTIFICATION_WM_GO_BACK_REQUEST:
+		get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+		
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	update_records()
