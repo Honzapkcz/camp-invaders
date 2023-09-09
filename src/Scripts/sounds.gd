81216281fe346c6ad1533f0f7bc214ba30246afd
Honzapkcz <honzapkc@gmail.com>
2023-09-09 14:09:02 +0200

    sounds.gd: fixed bug

--- src/Scripts/sounds.gd
+++ src/Scripts/sounds.gd
@@ -1,10 +1,12 @@
 extends Node
 
 
-
 func play(sound: String):
-	for audioPlayer in get_children():
+	var audioPlayers = get_children()
+	for audioPlayer in audioPlayers:
 		if audioPlayer.playing:
 			continue
 		audioPlayer.stream = load("res://Sounds/" + sound + ".mp3")
 		audioPlayer.play()
+		return
+	print_debug("dropping")
