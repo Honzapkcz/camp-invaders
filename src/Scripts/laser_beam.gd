763435d66d1391d5823b66d2057149dcf036fcbe
Honzapkcz <honzapkc@gmail.com>
2023-09-13 20:08:03 +0200

    Remade LaserBeam tree and made functional (untested)

--- src/Scripts/laser_beam.gd
+++ src/Scripts/laser_beam.gd
@@ -1,11 +1,6 @@
 extends AnimationPlayer
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_area_2d_body_entered(body):
+	if body is Player:
+		Global.emit_signal("hurt_player")
